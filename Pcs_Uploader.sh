245087f77c3a1dc7bb93a3da89ba377bbca7ddfe
zjhui <jiahui.tar.gz@gmail.com>
2014-02-19 14:52:21 +0800

    用JSON.sh来解析返回的json

--- Pcs_Uploader.sh
+++ Pcs_Uploader.sh
@@ -16,6 +16,19 @@ function trim {
     sed -e 's/^[[:space:]]*//g' -e 's/[[:space:]]*\$//g'
 }
 
+# 判断是目录还是文件
+function pcs_stat {
+    local FILE="$1"
+    curl -s --show-error --globoff -i -k -L "https://pcs.baidu.com/rest/2.0/pcs/file?method=meta&access_token=$ACCESS_TOKEN&path=/apps/$APP_FOLDER/$FILE" -o $RESPONSE_FILE
+    if grep -q "HTTP/1.1 404 Not Found" $RESPONSE_FILE;then
+        eerror "文件不存在，请检查输入的文件名是否正确或该文件是否已经被删除！"
+    fi
+    grep -q "^HTTP/1.1 200 OK" $RESPONSE_FILE > /dev/null 2>&1
+    if [ $? -eq 0 ];then
+        local IS_DIR=
+    fi
+}
+
 # ACCESS_TOKEN过期后重新刷新
 function refresh_access_token() {
     curl -k -L -d "grant_type=refresh_token&refresh_token=$REFRESH_TOKEN&client_id=$APIKEY&client_secret=$SECRETKEY" \
@@ -83,6 +96,27 @@ function pcs_mkdir {
     fi
 }
 
+# 删除文件/目录
+function pcs_delete {
+    local DST_DIR="$1"
+    read -p "删除/apps/$APP_FOLDER/$DST_DIR ? [y/n] " ans
+    case "${ans}" in
+        [Yy][Ee][Ss]|[Yy]|"")
+            : ;;
+        *)
+            einfo "Right. Aborting..."
+            exit 1
+            ;;
+    esac
+    curl -s --show-error --globoff -i -k -L "https://pcs.baidu.com/rest/2.0/pcs/file?method=delete&access_token=$ACCESS_TOKEN&path=/apps/$APP_FOLDER/$DST_DIR" -o $RESPONSE_FILE
+    grep -q "^HTTP/1.1 200 OK" $RESPONSE_FILE > /dev/null 2>&1
+    if [ $? -eq 0 ];then
+        einfo "$DST_DIR删除成功！"
+    else
+        eerror "删除失败，请查看$RESPONSE_FILE! "
+    fi
+}
+
 # 完成一些初始化配置
 if [ -f $CONFIG_FILE ];then
     source $CONFIG_FILE
@@ -103,11 +137,16 @@ else
         fi
     done
     
+    # 获得当前程序运行的路径,from: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+    DIR="$( cd "$( dirname "$0" )" && pwd )" 
+
     # 通过APIKEY获得device_code和user_code
     einfo "获取code中..."
     curl -s -k -L -d "client_id=$APIKEY&response_type=device_code&scope=basic,netdisk" $API_CODE_URL -o $RESPONSE_FILE 
-    DEVICE_CODE=`awk -F\" '{print $4}' $RESPONSE_FILE`
-    USER_CODE=`awk -F\" '{print $8}' $RESPONSE_FILE`
+#    DEVICE_CODE=`awk -F\" '{print $4}' $RESPONSE_FILE`
+#    USER_CODE=`awk -F\" '{print $8}' $RESPONSE_FILE`
+    DEVICE_CODE=${tail -1 $RESPONSE_FILE | bash /$DIR/JSON.sh/JSON.sh -l | awk '/device_code/{print $2}' | tr -d \"}
+    USER_CODE=${tail -1 $RESPONSE_FILE | bash /$DIR/JSON.sh/JSON.sh -l | awk '/user_code/{print $2}' | tr -d \"}
     while (true);do
         einfo "请打开链接 http://openapi.baidu.com/device?code=$USER_CODE&display=page&force_login= 点击授权。"
         einfo "完成后请按回车"
@@ -115,10 +154,12 @@ else
     
         einfo "获取access_token和refresh_token..."
         curl -s -k -L -d "grant_type=device_token&code=$DEVICE_CODE&client_id=$APIKEY&client_secret=$SECRETKEY" $API_TOKEN_URL -o $RESPONSE_FILE
-        cat $RESPONSE_FILE
-        REFRESH_TOKEN=`awk -F\" '{print $6}' $RESPONSE_FILE`
+#       cat $RESPONSE_FILE
+#       REFRESH_TOKEN=`awk -F\" '{print $6}' $RESPONSE_FILE`
+        REFRESH_TOKEN=${tail -1 $RESPONSE_FILE | bash /$DIR/JSON.sh/JSON.sh -l | awk '/refresh_token/{print $2}' | tr -d \"}
         echo $REFRESH_TOKEN
-        ACCESS_TOKEN=`awk -F\" '{print $10}' $RESPONSE_FILE`
+#        ACCESS_TOKEN=`awk -F\" '{print $10}' $RESPONSE_FILE`
+        ACCESS_TOKEN=${tail -1 $RESPONSE_FILE | bash /$DIR/JSON.sh/JSON.sh -l | awk '/access_token/{print $2}' | tr -d \"}
         echo $ACCESS_TOKEN
         if [[ $REFRESH_TOKEN != "" && $ACCESS_TOKEN != "" ]];then
             einfo "ok\n"
@@ -158,6 +199,14 @@ case $COMMAND in
     ;;
     mkdir)
         DIR_DST=$ARG1
-        pcs_mkdir $DIR_DST
+        pcs_mkdir "$DIR_DST"
+    ;;
+    delete)
+        FILE_DST=$ARG1
+        pcs_delete "$FILE_DST"
+    ;;
+    stat)
+        FILE_DST=$ARG1
+        pcs_stat "$FILE_DST"
     ;;
 esac
